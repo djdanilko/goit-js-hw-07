4588102fed16daa2716d5ba66355f17f3a4a03c6
djdanilko <djdanilko02@gmail.com>
2026-03-19 18:06:31 +0100

    tsks-3,4

---
+++ js/task-3.js
@@ -0,0 +1,11 @@
+const textInput = document.querySelector("#name-input");
+const output = document.querySelector("#name-output");
+
+textInput.addEventListener("input", (event) => {
+  const value = event.currentTarget.value.trim();
+  if (value === "") {
+    output.textContent = "Anonymous";
+  } else {
+    output.textContent = value;
+  }
+});
--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,39 @@
+// const form = document.querySelector("form");
 
+// const loginPassword = {};
+
+// form.addEventListener("submit", (e) => {
+//   const value = input.value.trim();
+
+//   if (value === "") {
+//     e.preventDefault();
+//     alert("All form fields must be filled in");
+//     return;
+//   } else {
+//     loginPassword.email = form.elements.email.value;
+//     loginPassword.password = form.elements.password.value;
+//   }
+// });
+
+// console.log(loginPassword);
+
+const form = document.querySelector("form");
+
+const loginPassword = {};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const email = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim();
+
+  if (email === "" || password === "") {
+    alert("All form fields must be filled in");
+    return;
+  }
+
+  loginPassword.email = email;
+  loginPassword.password = password;
+
+  console.log(loginPassword);
+});
